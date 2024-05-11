307c94f5adedf8a87552a875619dd3f17ce9af40
Farhi oussama baha eddine <farhioussama1969@gmail.com>
2024-05-11 11:34:07 +0100

    my personnel information's screen ui finished

--- lib/app/core/services/geocoding_service.dart
+++ lib/app/core/services/geocoding_service.dart
@@ -4,16 +4,12 @@ import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_google_maps_webservices/geocoding.dart';
 import 'package:flutter_google_maps_webservices/places.dart';
 import 'package:get/get.dart';
-import 'package:flutter_google_maps_webservices/src/core.dart'
-    as googleMapsWebServices;
+import 'package:flutter_google_maps_webservices/src/core.dart' as googleMapsWebServices;
 
 class GeocodingService {
-  static Future<String?> getPlaceMarkFromCoordinates(
-      double lat, double lng) async {
-    final places =
-        GoogleMapsGeocoding(apiKey: FlutterConfig.get('GOOGLE_MAPS_API_KEY'));
-    GeocodingResponse response = await places
-        .searchByLocation(googleMapsWebServices.Location(lat: lat, lng: lng));
+  static Future<String?> getPlaceMarkFromCoordinates(double lat, double lng) async {
+    final places = GoogleMapsGeocoding(apiKey: FlutterConfig.get('GOOGLE_MAPS_API_KEY'));
+    GeocodingResponse response = await places.searchByLocation(googleMapsWebServices.Location(lat: lat, lng: lng));
 
     response.results.first.addressComponents.forEach((element) {
       log('address component::: ${element.toJson()}');
@@ -24,12 +20,9 @@ class GeocodingService {
     return response.results.first.formattedAddress;
   }
 
-  static Future<List<PlacesSearchResult>> getPlacesSuggestionsFromText(
-      String searchText) async {
-    final places =
-        GoogleMapsPlaces(apiKey: FlutterConfig.get('GOOGLE_MAPS_API_KEY'));
-    PlacesSearchResponse response = await places.searchByText(searchText,
-        language: Get.locale?.languageCode);
+  static Future<List<PlacesSearchResult>> getPlacesSuggestionsFromText(String searchText) async {
+    final places = GoogleMapsPlaces(apiKey: FlutterConfig.get('GOOGLE_MAPS_API_KEY'));
+    PlacesSearchResponse response = await places.searchByText(searchText, language: Get.locale?.languageCode);
     return response.results;
   }
 }
