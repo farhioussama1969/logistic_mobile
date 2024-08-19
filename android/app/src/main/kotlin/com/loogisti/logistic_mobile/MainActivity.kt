9f73999696fc8b26fb4c8e7ed86663739ae4ced7
Farhi oussama baha eddine <farhioussama1969@gmail.com>
2024-08-19 11:03:27 +0100

    ios configuration finished

---
+++ android/app/src/main/kotlin/com/loogisti/logistic_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.loogisti.logistic_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,10 @@
 import UIKit
 import Flutter
+import Firebase
+import FirebaseMessaging
+import flutter_local_notifications
+import GoogleMaps
+import flutter_config
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +12,25 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+              GeneratedPluginRegistrant.register(with: registry)
+          }
+
+          if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+          }
+          FirebaseApp.configure()
+          GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "GOOGLE_MAPS_API_KEY"))
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+   override func application(_ application: UIApplication,
+              didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+               Messaging.messaging().apnsToken = deviceToken
+               print("Token: \(deviceToken)")
+               super.application(application,
+               didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+             }
 }
